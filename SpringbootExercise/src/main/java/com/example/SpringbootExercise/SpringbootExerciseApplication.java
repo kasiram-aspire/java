f8e51d3e6ffab8ac859e390be0ce553cc2e7c5f9
kasiram-aspire <kasiram.palpandi@aspiresys.com>
2025-01-22 09:21:01 +0530

    initial commit

---
+++ SpringbootExercise/src/main/java/com/example/SpringbootExercise/SpringbootExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExerciseApplication.class, args);
+	}
+
+}
