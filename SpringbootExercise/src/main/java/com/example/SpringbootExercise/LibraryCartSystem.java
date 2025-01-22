f731d341b7382068e56a2d22185949302b04338c
kasiram-aspire <kasiram.palpandi@aspiresys.com>
2025-01-22 18:18:21 +0530

    implementation of dependency injector,Components,ApplicatonContext

---
+++ SpringbootExercise/src/main/java/com/example/SpringbootExercise/LibraryCartSystem.java
@@ -0,0 +1,107 @@
+package com.example.SpringbootExercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/*
+  This program is a Library Cart System built using Spring Boot. 
+  The program simulates the management of a library where users can add books to a list (cart),
+   remove books from it, and display the current list of books in the cart. 
+   It uses the Spring Framework to manage the components and make the program modular and easily testable.
+ */
+
+class librarydetails
+{   
+	public int id;
+	public String bookname;
+	public librarydetails(int id,String name) //constructor for library details
+	{
+		this.id=id;
+		this.bookname=name;
+	}
+	@Override
+	public String toString() {
+		return "librarydetails [id=" + id + ", bookname=" + bookname + "]";
+	}
+	
+}
+
+@Component
+class librarymanagement
+{
+	public void display(librarydetails lbd)  //display opertaion take place here
+	{
+		System.out.println(lbd);
+	}
+}
+@SpringBootApplication
+public class LibraryCartSystem {
+    public static void main(String[] args) {
+    	List<librarydetails> library=new ArrayList<>();
+        ApplicationContext context = SpringApplication.run(LibraryCartSystem.class, args); 
+        librarymanagement lbm=context.getBean(librarymanagement.class);  // it stores the object for the libararymanagement class
+        Scanner sc=new Scanner(System.in);
+        while(true)   //it will loop upto pressing 4
+		{   
+			System.out.println("press 1 for Adding book");
+			System.out.println("press 2 for Remove book from cart");
+			System.out.println("press 3 for display");
+			System.out.println("press 4 forExit");
+			System.out.println("-------------------------------------------------");
+			System.out.println("Enter the number:");
+			int num=sc.nextInt();
+			sc.nextLine();
+			switch(num)
+			{
+			   case 1:     //adding data into the libarary details constructor
+				   System.out.println("Enter the book id :");
+					int idnum=sc.nextInt();
+					sc.nextLine();
+					System.out.println("Enter the book name:");
+					String bookname=sc.nextLine();
+					library.add(new librarydetails(idnum,bookname));
+					break;
+			   case 2:  //based on the id of the book it will remove the value from the array list
+				   System.out.println("Enter the book id you want to remove :");
+					int id=sc.nextInt();
+					sc.nextLine();
+					for(int index = 0;index<library.size();index++)
+					{   
+						librarydetails value=library.get(index);
+						if(value.id==id)
+						{
+							library.remove(index);
+						}
+						
+					}
+					break;
+			   case 3: //it display the all books in the library
+				   System.out.println("-------------------------------------------------");
+				    for(librarydetails lbd:library)
+				    {
+				    	lbm.display(lbd);
+				    }
+				    System.out.println("-------------------------------------------------");
+					break;
+			   
+			   case 4:  //exit loop it initialized here
+				   System.out.println("....................Exited...................");
+				   return;
+			   default:
+				   System.out.println("*****invalid input******");
+				   
+			}
+			
+		}	
+        
+        
+        
+    }
+}
--- SpringbootExercise/src/main/java/com/example/SpringbootExercise/SpringbootExerciseApplication.java
+++ SpringbootExercise/src/main/java/com/example/SpringbootExercise/SpringbootExerciseApplication.java
@@ -1,13 +1,57 @@
 package com.example.SpringbootExercise;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Primary;
+interface Computer
+{
+	public void display();
+}
+@Component 
+class Desktop implements Computer
+{
+	public void display()
+	{
+		System.out.println("computer showed");
+	}
+}
+
+@Component
+class Laptop implements Computer
+{
+	public void display()
+	{
+		System.out.println("laptop showed");
+	}
+}
+@Component
+class demo
+{   
+	Computer com;
+	@Autowired 
+	@Qualifier("desktop")
+	public void setlpatop(Computer obj)
+	{
+		com=obj;
+	}
+	public void show()
+	{
+		System.out.println("demo showed");
+		com.display();
+	}
+}
 
 @SpringBootApplication
 public class SpringbootExerciseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootExerciseApplication.class, args);
+		ApplicationContext context=SpringApplication.run(SpringbootExerciseApplication.class, args);
+		demo obj=context.getBean(demo.class);
+		obj.show();
 	}
 
 }
