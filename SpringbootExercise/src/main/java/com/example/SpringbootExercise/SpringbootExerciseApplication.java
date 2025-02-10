305614b4318e9046fa0737f236a531bfa2506347
kasiram-aspire <kasiram.palpandi@aspiresys.com>
2025-02-10 18:55:02 +0530

    implemented logging using log4j

--- SpringbootExercise/src/main/java/com/example/SpringbootExercise/SpringbootExerciseApplication.java
+++ SpringbootExercise/src/main/java/com/example/SpringbootExercise/SpringbootExerciseApplication.java
@@ -1,5 +1,7 @@
 package com.example.SpringbootExercise;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,9 +11,19 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 //@ComponentScan("com.example.SpringbootExercise.*")
 public class SpringbootExerciseApplication {
-
+    public static Logger logger=LogManager.getLogger( SpringbootExerciseApplication.class);
+    
+    public static void process()
+    {   System.out.println("-------------------logger info-------------------------------------------------");
+	   	logger.trace("This is a TRACE log message");
+	    logger.debug("This is a DEBUG log message");
+	    logger.info("This is an INFO log message");
+	    logger.warn("This is a WARN log message");
+	    logger.error("This is an ERROR log message");
+    }
 	public static void main(String[] args) {
 		ApplicationContext context=SpringApplication.run(SpringbootExerciseApplication.class, args);
+	    process();
 //		demo obj=context.getBean(demo.class);
 //		obj.show();
 	}
--- SpringbootExercise/src/main/java/com/example/SpringbootExercise/controller/EmployeeController.java
+++ SpringbootExercise/src/main/java/com/example/SpringbootExercise/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.example.SpringbootExercise.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,24 +21,32 @@ import jakarta.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("/emp")
 public class EmployeeController {
+   private Logger logger=LogManager.getLogger(EmployeeController.class);
+   
    @Autowired
    EmployeeService employeeservice;
    
    @PostMapping("/add")
    public String add(@RequestBody Employee emp )
    {
+	   logger.info("Received request to add employee:",emp);   
 	   employeeservice.addEmployeeWithAdress(emp);
+	   logger.info("Employee added successfully:",emp);
 	   return"added";
    }
    @GetMapping("/get")
    public List<Employee> show()
-   {
+   {   logger.info("Fetching all employees");
+       List<Employee> employees = employeeservice.showemployee();
+       logger.info("Total employees retrieved: {}", employees.size());
 	   return employeeservice.showemployee();
+	   
    }
    @GetMapping("/csrf-token")
 	public CsrfToken getCsrfToken(HttpServletRequest request)
-	{
-		return (CsrfToken) request.getAttribute("_csrf");
+	{  CsrfToken csrftoken= (CsrfToken) request.getAttribute("_csrf");
+	   logger.debug("Retrieved CSRF token: {}", csrftoken);
+		return csrftoken;
 	}
    @GetMapping("/session")
   	public String getsessionid(HttpServletRequest request)
@@ -45,7 +55,9 @@ public class EmployeeController {
   	}
    @GetMapping("/get/{name}")
  	public List<Employee> getsessionid(@PathVariable String name)
- 	{
+ 	{    logger.info("Fetching employees with name: {}", name);
+ 		List<Employee> employees = employeeservice.getByName(name);
+ 		logger.info("Total employees retrieved for name {}: {}", name, employees.size());
  		return employeeservice.getByName(name);
  	}
 }
